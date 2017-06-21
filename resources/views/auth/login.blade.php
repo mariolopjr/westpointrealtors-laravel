fc84e8b2f2fb5cc290b8957b8e36fcffb7461464
Mario Lopez <mario@techmunchies.net>
2017-06-20 20:05:44 -0400

    More login work, slide in, some styling

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -23,8 +23,8 @@
 
         <div class="hero-body">
             <div class="container has-text-centered">
-                <h1 class="title">Thanks for coming back!</h1>
-                <div class="card column is-half is-offset-one-quarter add-shadow">
+                <h1 class="title drop-in-transition-delay2">Thanks for coming back!</h1>
+                <div class="card column is-half is-offset-one-quarter add-shadow-2x drop-in-transition">
                     <header class="card-header">
                         <p class="card-header-title has-text-centered">
                             Please login
@@ -36,19 +36,49 @@
 
                             <div class="field">
                                 <label class="label" for="email">Email</label>
-                                <p class="control">
-                                    <input class="input" type="email" name="email" id="email" required />
+                                <p class="control has-icons-left{{ $errors->has('email') ? ' has-icons-right' : '' }}">
+                                    <input class="input{{ $errors->has('email') ? ' is-danger' : '' }}" type="email" name="email" id="email" value="{{ old('email') }}" placeholder="Email Address" required autofocus />
+
+                                    <span class="icon is-small is-left">
+                                        <i class="fa fa-envelope"></i>
+                                    </span>
+
+                                    @if ($errors->has('email'))
+                                        <span class="icon is-small is-right">
+                                            <i class="fa fa-warning"></i>
+                                        </span>
+                                    @endif
                                 </p>
+                                @if ($errors->has('email'))
+                                    <p class="help is-danger">This email is invalid</p>
+                                @endif
                             </div>
 
                             <div class="field">
                                 <label class="label" for="password">Password</label>
-                                <p class="control">
-                                    <input class="input" type="password" name="password" id="password" required />
+                                <p class="control has-icons-left{{ $errors->has('password') ? ' has-icons-right' : '' }}">
+                                    <input class="input square-corners{{ $errors->has('password') ? ' is-danger' : '' }}" type="password" name="password" id="password" placeholder="Password" required />
+
+                                    <span class="icon is-small is-left">
+                                        <i class="fa fa-lock"></i>
+                                    </span>
+
+                                    @if ($errors->has('password'))
+                                        <span class="icon is-small is-right">
+                                            <i class="fa fa-warning"></i>
+                                        </span>
+                                    @endif
                                 </p>
+                                @if ($errors->has('password'))
+                                    <p class="help is-danger">This password is invalid</p>
+                                @endif
                             </div>
 
-                            <a type="submit" class="button is-primary">Primary</a>
+                            <a class="" href="{{ route('password.request') }}">
+                                Forgot Your Password?
+                            </a>
+
+                            <button type="submit" class="button is-primary square-corners login-btn">Login</button>
                         </form>
                     </div>
                 </div>
