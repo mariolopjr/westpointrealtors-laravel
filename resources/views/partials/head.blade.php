9470c2305c8bd6d18a7456774494e102219e16f7
Mario Lopez <mario@techmunchies.net>
2017-09-28 20:45:24 -0400

    Removed dropzone

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -11,7 +11,6 @@
     <!-- Stylesheets -->
     {!! HTML::style(asset('https://fonts.googleapis.com/css?family=Raleway:100,300,400,600'), array('type' => 'text/css', 'rel' => 'stylesheet')) !!}
     {!! HTML::script(asset('https://use.fontawesome.com/ffe4b59639.js'), array('type' => 'text/javascript')) !!}
-    {!! HTML::style(asset('https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.1.1/min/dropzone.min.css'), array('type' => 'text/css', 'rel' => 'stylesheet', 'integrity' => 'sha256-e47xOkXs1JXFbjjpoRr1/LhVcqSzRmGmPqsrUQeVs+g=', 'crossorigin' => 'anonymous')) !!}
     {!! HTML::style(asset('css/app.css'), array('type' => 'text/css', 'rel' => 'stylesheet')) !!}
 
     @yield('addStylesheets')
